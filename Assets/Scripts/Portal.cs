91bb9bca47c6d77cad82301ea6f36e09731bf545
Colton Henderson <coltonhenderson12@hotmail.com>
2022-11-27 17:44:43 -0600

    Added other scenes and created portals for home world and castle world

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    [SerializeField] string level = "Home World Scene";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(level);
+        }
+    }
+}
